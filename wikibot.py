6d764c3f062b7005531271bb93e120440f163499
Kalpana Gollapalli <kalpana@Kalpanas-MacBook-Pro.local>
2026-05-06 12:26:47 -0700

    fix

--- wikibot.py
+++ wikibot.py
@@ -6,9 +6,9 @@ from my_lib.bot import scrape
 @click.command()
 @click.option("--name", default="Microsoft", help="The Wikipedia page to scrape.")
 @click.option("--length", default=2, help="Number of sentences to return.")
-def cli(name: str, length: int):
+def cli(name: str, length: int):  # pylint: disable=E1120
     """Scrape a Wikipedia summary."""
-    result = scrape(name, length)  # pylint: disable=no-value-for-parameter
+    result = scrape(name, length)
     click.echo(click.style(result, fg="blue", bg="white", bold=True))
 
 
