ca9aecf5a89451d1ff43394108e1a94c333b63ea
Kalpana Gollapalli <kalpana@Kalpanas-MacBook-Pro.local>
2026-05-06 12:17:21 -0700

    fix for No value for argument 'name' in function call

--- wikibot.py
+++ wikibot.py
@@ -6,9 +6,9 @@ from my_lib.bot import scrape
 @click.command()
 @click.option("--name", default="Microsoft", help="The Wikipedia page to scrape.")
 @click.option("--length", default=2, help="Number of sentences to return.")
-def cli(name, length):
+def cli(name: str, length: int):
     """Scrape a Wikipedia summary."""
-    result = scrape(name, length)
+    result = scrape(name, length)  # pylint: disable=no-value-for-parameter
     click.echo(click.style(result, fg="blue", bg="white", bold=True))
 
 
